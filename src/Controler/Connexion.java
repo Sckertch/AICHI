3ca51abe6e35a6ca9dd505c6c737a140be341ce0
mathis <mathis>
2024-11-25 14:30:23 -0400

    Affichage page acceuil

--- src/Controler/Connexion.java
+++ src/Controler/Connexion.java
@@ -3,6 +3,8 @@ package Controler;
 import java.awt.event.ActionEvent;  
 import java.awt.event.ActionListener; 
 import javax.swing.JOptionPane; // Importation de JOptionPane
+
+import views.FAcceuilView;
 import views.FConnexionView;
 import modele.User;
 import modele.UserDAO;
@@ -23,25 +25,29 @@ public class Connexion {
     }
 
     private void handleLogin() {
-        User newUser   = new User();
-        newUser .setEmail(view.getLogin());
-        newUser .setMotDePasse(view.getMotDePasse());
+        User newUser = new User();
+        newUser.setEmail(view.getLogin());
+        newUser.setMotDePasse(view.getMotDePasse());
 
         try {
-            boolean isValidUser  = userdao.verifUser (newUser );
-            if (isValidUser ) {
+            boolean isValidUser = userdao.verifUser(newUser);
+            if (isValidUser) {
                 System.out.println("Connexion réussie");
                 JOptionPane.showMessageDialog(view, "Connexion réussie", "Succès", JOptionPane.INFORMATION_MESSAGE);
+                
                 // Proceed with the application flow, e.g., open the main application window
+                FAcceuilView accueilView = new FAcceuilView();
+                accueilView.setVisible(true);
+                
+                view.dispose();
+                
             } else {
                 System.out.println("Identifiants incorrects, veuillez réessayer.");
                 JOptionPane.showMessageDialog(view, "Identifiants incorrects, veuillez réessayer.", "Erreur", JOptionPane.ERROR_MESSAGE);
-                // Optionally, show a message dialog to the user
             }
         } catch (Exception e) {
             System.out.println("Erreur lors de la connexion : " + e.getMessage());
             JOptionPane.showMessageDialog(view, "Erreur lors de la connexion : " + e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
-            // Optionally, log the error or show a message dialog to the user
         }
     }
 }
